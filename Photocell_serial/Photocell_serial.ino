c7d5a8c177f78e3fb24cc2c7a26fe328df84827d
ralphrtm <37366255+ralphrtm@users.noreply.github.com>
2018-06-28 13:01:00 +0200

    Add files via upload

---
+++ Photocell_serial/Photocell_serial.ino
@@ -0,0 +1,23 @@
+//www.elegoo.com
+//2016.12.9
+
+
+#define pc A7
+
+int pcVal;
+
+void setup() 
+{
+  pinMode(pc, INPUT);
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() 
+{
+  pcVal = analogRead(pc);
+  Serial.write(pcVal);
+  delay(100);
+}
+
+
